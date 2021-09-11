d2ecaa41a5f38d70e7d59ff433d52977a06bad60
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-11 16:47:43 +0100

    CurrentCountry

---
+++ UI/CurrentCountry.jsx
@@ -0,0 +1,9 @@
+import React, { } from 'react'
+import styles from './../styles/Home.module.css'
+
+const CurrentCountry = () => {
+    return (
+        <div></div>
+    )
+}
+export default CurrentCountry
