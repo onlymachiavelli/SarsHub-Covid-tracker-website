daafc93fdea577d672179df7f87e3d0624396a1d
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-14 13:06:33 +0100

    fetched + style new style file

--- pages/world.js
+++ pages/world.js
@@ -1,5 +1,13 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+const AllWorld = async () => {
+    const res = await axios.get("https://disease.sh/v3/covid-19/countries/").then(
+        res => res.data
+    )
+    return res
+}
 
+AllWorld().then(res => console.log(res))
 const World = () => {
     return (
         <div></div>
