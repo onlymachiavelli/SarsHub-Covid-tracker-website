468ccff77fe8cbff9e4f35546a5b10d7765d37c8
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-15 20:50:40 +0100

    CovSearch

--- src/UI/search.jsx
+++ src/UI/search.jsx
@@ -1,6 +1,6 @@
 import React, { } from 'react'
 import { Search } from "./SVG"
-import styles from './../../styles/Home/module.css'
+import styles from './../../styles/Home.module.css'
 const CovidSearch = (props) => {
     return (
         <div className={styles.searchResult} style={{ display: props.visibility }}>
