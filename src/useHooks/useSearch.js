a8e6829196bef7ce9468b946545db34d5fe5e4e6
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-18 16:29:39 +0100

    mis

--- src/useHooks/useSearch.js
+++ src/useHooks/useSearch.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import getcovByCountr from './../apiCall/covbycountry'
+const Low = (ele) => {
+    let res = ""
+    for (let i = 0; i < ele.length; i++) res += ele[i].toLowerCase()
+    if (res === "israel") return "palestine"
 
+    return ele
+}
 const useSearch = () => {
     const [result, setResult] = useState({})
     const [countrysearch, setCountrySearch] = useState("")
 
     const handleSearch = () => {
+        setCountrySearch(Low(countrysearch))
+        console.log(Low(countrysearch))
         if (countrysearch)
-            getcovByCountr(countrysearch).then(
+
+            getcovByCountr(Low(countrysearch)).then(
                 res => {
                     setResult({
                         totalcases: res.cases,
@@ -23,6 +32,8 @@ const useSearch = () => {
                     })
                 }
             )
+
+
     }
 
     return { result, handleSearch, countrysearch, setCountrySearch }
