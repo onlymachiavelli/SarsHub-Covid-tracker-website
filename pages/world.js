bacac16344c1bf46c27016293e3f9fbe851905e9
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-18 19:03:27 +0100

    fetch all the world

--- pages/world.js
+++ pages/world.js
@@ -4,7 +4,35 @@ import { WorldCases } from './../src/apiCall/worldcovid'
 
 const World = () => {
     return (
-        <div className={styles.world}></div>
+        <div className={styles.world}>
+            <table>
+                <tr>
+                    <th>1</th>
+                    <th>Tunisia</th>
+                    <th><img src={"https://www.countryflags.io/TN/flat/64.png"} /></th>
+                    <th>Total Cases</th>
+                    <th>todayCases</th>
+                    <th>deaths</th>
+                    <th>todayDeaths</th>
+                    <th>recovered</th>
+                    <th>todayRecovered</th>
+                    <th>active</th>
+                    <th>critical</th>
+                    <th>casesPerOneMillion</th>
+                    <th>deathsPerOneMillion</th>
+                    <th>tests</th>
+                    <th>testsPerOneMillion</th>
+                    <th>population</th>
+                    <th>oneCasePerPeople</th>
+                    <th>oneDeathPerPeople</th>
+                    <th>activePerOneMillion</th>
+                    <th>recoveredPerOneMillion</th>
+                    <th>criticalPerOneMillion</th>
+
+
+                </tr>
+            </table>
+        </div>
     )
 }
 export default World
