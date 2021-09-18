407f618de8a2b5a2112e69b89dd80e3ae09ac5cf
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-18 19:55:35 +0100

    Langs

--- pages/world.js
+++ pages/world.js
@@ -1,34 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import styles from './../styles/World.module.css'
 import { WorldCases } from './../src/apiCall/worldcovid'
-
+import { Language } from './../src/UI/language'
 const World = () => {
+    const [lan, setLan] = useState(Language.English)
     return (
         <div className={styles.world}>
             <table>
                 <tr>
-                    <th>LVL</th>
-                    <th>COUNTRY</th>
-                    <th>CONTINENT</th>
-                    <th>FLAG</th>
-                    <th>population</th>
-                    <th>Total Cases</th>
-                    <th>recovered</th>
-                    <th>deaths</th>
-                    <th>todayCases</th>
-                    <th>todayRecovered</th>
-                    <th>todayDeaths</th>
-                    <th>active</th>
-                    <th>critical</th>
-                    <th>casesPerOneMillion</th>
-                    <th>deathsPerOneMillion</th>
-                    <th>tests</th>
-                    <th>testsPerOneMillion</th>
-                    <th>oneCasePerPeople</th>
-                    <th>oneDeathPerPeople</th>
-                    <th>activePerOneMillion</th>
-                    <th>recoveredPerOneMillion</th>
-                    <th>criticalPerOneMillion</th>
+                    <th>{lan.table.lvl}</th>
+                    <th>{lan.table.country}</th>
+                    <th>{lan.table.continent}</th>
+                    <th>{lan.table.flag}</th>
+                    <th>{lan.table.population}</th>
+                    <th>{lan.table.tcases}</th>
+                    <th>{lan.table.trec}</th>
+                    <th>{lan.table.tdcases}</th>
+                    <th>{lan.table.tdcases}</th>
+                    <th>{lan.table.tdrec}</th>
+                    <th>{lan.table.tddeaths}</th>
+                    <th>{lan.table.active}</th>
+                    <th>{lan.table.critical}</th>
+                    <th>{lan.table.cpom}</th>
+                    <th>{lan.table.dpom}</th>
+
+                    <th>{lan.table.apom}</th>
+                    <th>{lan.table.rpom}</th>
+                    <th>{lan.table.crpom}</th>
+
+                    <th>{lan.table.tests}</th>
+                    <th>{lan.table.tpom}</th>
+                    <th>{lan.table.ocpp}</th>
+                    <th>{lan.table.odpp}</th>
+
                 </tr>
             </table>
         </div>
