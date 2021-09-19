44674ee87cf0fa1cb97e14120fe7be8ed996613e
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-19 18:40:43 +0100

    Fixing Bug

--- src/useHooks/useSearch.js
+++ src/useHooks/useSearch.js
@@ -1,42 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import getcovByCountr from './../apiCall/covbycountry'
-const Low = (ele) => {
-    let res = ""
-    for (let i = 0; i < ele.length; i++) res += ele[i].toLowerCase()
-    if (res === "israel") return "palestine"
+import React, { useState, useEffect } from "react"
+import getcovByCountr from "./../apiCall/covbycountry"
+const Isr = (ele) => {
+  let res = ele.toLowerCase()
+  if (res === "israel") return "palestine"
 
-    return ele
+  return ele
 }
 const useSearch = () => {
-    const [result, setResult] = useState({})
-    const [countrysearch, setCountrySearch] = useState("")
+  const [result, setResult] = useState({})
+  const [countrysearch, setCountrySearch] = useState("")
 
-    const handleSearch = () => {
-        setCountrySearch(Low(countrysearch))
-        console.log(Low(countrysearch))
-        if (countrysearch)
+  const handleSearch = () => {
+    setCountrySearch(Isr(countrysearch))
+    if (countrysearch)
+      getcovByCountr(Isr(countrysearch)).then((res) => {
+        setResult({
+          totalcases: res.cases,
+          totaldeaths: res.deaths,
+          totalrecovered: res.recovered,
+          todaycases: res.todayCases,
+          todaydeaths: res.todayDeaths,
+          todayrecovered: res.todayRecovered,
+          activecases: res.active,
+          critical: res.critical,
+          flag: res.countryInfo.iso2,
+          country:
+            res.country == "Tunisia" ? res.country + " Lkalba" : res.country,
+        })
+      })
+  }
 
-            getcovByCountr(Low(countrysearch)).then(
-                res => {
-                    setResult({
-                        totalcases: res.cases,
-                        totaldeaths: res.deaths,
-                        totalrecovered: res.recovered,
-                        todaycases: res.todayCases,
-                        todaydeaths: res.todayDeaths,
-                        todayrecovered: res.todayRecovered,
-                        activecases: res.active,
-                        critical: res.critical,
-                        flag: res.countryInfo.iso2,
-                        country: res.country == "Tunisia" ? res.country + " Lkalba" : res.country
-                    })
-                }
-            )
-
-
-    }
-
-    return { result, handleSearch, countrysearch, setCountrySearch }
+  return { result, handleSearch, countrysearch, setCountrySearch }
 }
 
 export default useSearch
