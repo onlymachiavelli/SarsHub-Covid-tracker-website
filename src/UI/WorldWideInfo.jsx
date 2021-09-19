f85d7e3397a3e1d9f9e3052493f70c4775d3b0a7
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-19 22:33:34 +0100

    no colors 0

--- src/UI/WorldWideInfo.jsx
+++ src/UI/WorldWideInfo.jsx
@@ -3,18 +3,26 @@ import styles from "./../../styles/Home.module.css"
 
 const WorldInfo = (props) => {
   return (
-    <div className={styles.worldinfo_container} >
+    <div className={styles.worldinfo_container}>
       <p className={styles.gl_title}>{props.titles["globecases"]}</p>
       <div className={styles.flex_info_container}>
         <div className={styles.worldinfocard}>
           <p id={styles.card_title}>{props.titles["totalcases"]}</p>
           <p className={styles.cardnum}>{props.tcases}</p>
-        </div>&nbsp;
-        <div className={styles.worldinfocard} id={styles.death_card}>
+        </div>
+        &nbsp;
+        <div
+          className={styles.worldinfocard}
+          id={Number(props.tdeaths) > 0 ? styles.death_card : ""}
+        >
           <p id={styles.card_title}>{props.titles["totaldeathcases"]}</p>
           <p className={styles.cardnum}>{props.tdeaths}</p>
-        </div>&nbsp;
-        <div className={styles.worldinfocard} id={styles.recovered_card}>
+        </div>
+        &nbsp;
+        <div
+          className={styles.worldinfocard}
+          id={Number(props.trec) > 0 ? styles.recovered_card : ""}
+        >
           <p id={styles.card_title}>{props.titles["totalrecoveredcases"]}</p>
           <p className={styles.cardnum}>{props.trec}</p>
         </div>
@@ -24,23 +32,38 @@ const WorldInfo = (props) => {
           <div className={styles.worldinfocard}>
             <p id={styles.card_title}>{props.titles["todaycases"]}</p>
             <p className={styles.cardnum}>+ {props.tdcases}</p>
-          </div>&nbsp;
-          <div className={styles.worldinfocard} id={styles.death_card}>
+          </div>
+          &nbsp;
+          <div
+            className={styles.worldinfocard}
+            id={Number(props.tddeaths) > 0 ? styles.death_card : ""}
+          >
             <p id={styles.card_title}>{props.titles["todaydeaths"]}</p>
             <p className={styles.cardnum}>+ {props.tddeaths}</p>
           </div>
           &nbsp;
-          <div className={styles.worldinfocard} id={styles.recovered_card}>
+          <div
+            className={styles.worldinfocard}
+            id={Number(props.tdrec) > 0 ? styles.recovered_card : ""}
+          >
             <p id={styles.card_title}>{props.titles["todayrecovered"]}</p>
             <p className={styles.cardnum}>+ {props.tdrec}</p>
-          </div>&nbsp;
+          </div>
+          &nbsp;
         </div>
         <div className={styles.flex_info_container}>
-          <div className={styles.worldinfocard}>
+          <div
+            className={styles.worldinfocard}
+            id={Number(props.active) > 0 ? styles.cases : ""}
+          >
             <p id={styles.card_title}>{props.titles["activecases"]}</p>
             <p className={styles.cardnum}>{props.active}</p>
-          </div>&nbsp;
-          <div className={styles.worldinfocard} id={styles.critical} >
+          </div>
+          &nbsp;
+          <div
+            className={styles.worldinfocard}
+            id={Number(props.crit) > 0 ? styles.critical : ""}
+          >
             <p id={styles.card_title}>{props.titles["critical"]}</p>
             <p className={styles.cardnum}>{props.crit}</p>
           </div>
