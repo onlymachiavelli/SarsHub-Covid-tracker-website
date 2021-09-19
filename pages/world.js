b56b2bb299240ac4a1472c7c899ecdec7d490b24
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-19 18:51:30 +0100

    MAPPING

--- pages/world.js
+++ pages/world.js
@@ -1,50 +1,71 @@
-import React, { useState, useEffect } from 'react'
-import styles from './../styles/World.module.css'
-import { WorldCases } from './../src/apiCall/worldcovid'
-import { Language } from './../src/UI/language'
+import React, { useState, useEffect } from "react"
+import styles from "./../styles/World.module.css"
+import { WorldCases } from "./../src/apiCall/worldcovid"
+import { Language } from "./../src/UI/language"
 const World = () => {
-    const [lan, setLan] = useState(Language.English)
-    const [datas, setDatas] = useState([])
-    WorldCases().then(
-        res => { setDatas(res) }
-    )
+  const [lan, setLan] = useState(Language.English)
+  const [datas, setDatas] = useState([])
+  WorldCases().then((res) => {
+    setDatas(res)
+  })
 
-    return (
-        <div className={styles.world}>
-            <table className={styles.table}>
-                <tr>
-                    <th>{lan.table.lvl}</th>
-                    <th>{lan.table.country}</th>
-                    <th>{lan.table.continent}</th>
-                    <th>{lan.table.flag}</th>
-                    <th>{lan.table.population}</th>
-                    <th>{lan.table.tcases}</th>
-                    <th>{lan.table.trec}</th>
-                    <th>{lan.table.tdcases}</th>
-                    <th>{lan.table.tdcases}</th>
-                    <th>{lan.table.tdrec}</th>
-                    <th>{lan.table.tddeaths}</th>
-                    <th>{lan.table.active}</th>
-                    <th>{lan.table.critical}</th>
-                    <th>{lan.table.cpom}</th>
-                    <th>{lan.table.dpom}</th>
-                    <th>{lan.table.apom}</th>
-                    <th>{lan.table.rpom}</th>
-                    <th>{lan.table.crpom}</th>
-                    <th>{lan.table.tests}</th>
-                    <th>{lan.table.tpom}</th>
-                    <th>{lan.table.ocpp}</th>
-                    <th>{lan.table.odpp}</th>
-                </tr>
-                <tr>
-                    {
-                        datas.map(res => {
-
-                        })
-                    }
-                </tr>
-            </table>
-        </div>
-    )
+  return (
+    <div className={styles.world}>
+      <table className={styles.table}>
+        <tr>
+          <th>{lan.table.lvl}</th>
+          <th>{lan.table.country}</th>
+          <th>{lan.table.continent}</th>
+          <th>{lan.table.flag}</th>
+          <th>{lan.table.population}</th>
+          <th>{lan.table.tcases}</th>
+          <th>{lan.table.trec}</th>
+          <th>{lan.table.tdcases}</th>
+          <th>{lan.table.tdcases}</th>
+          <th>{lan.table.tdrec}</th>
+          <th>{lan.table.tddeaths}</th>
+          <th>{lan.table.active}</th>
+          <th>{lan.table.critical}</th>
+          <th>{lan.table.cpom}</th>
+          <th>{lan.table.dpom}</th>
+          <th>{lan.table.apom}</th>
+          <th>{lan.table.rpom}</th>
+          <th>{lan.table.crpom}</th>
+          <th>{lan.table.tests}</th>
+          <th>{lan.table.tpom}</th>
+          <th>{lan.table.ocpp}</th>
+          <th>{lan.table.odpp}</th>
+        </tr>
+        {datas.map((arr, idx) => {
+          return (
+            <tr key={`arr-${idx}`}>
+              <th>{idx + 1}</th>
+              <th>{arr.country}</th>
+              <th>{arr.continent}</th>
+              <th>{lan.table.flag}</th>
+              <th>{arr.population}</th>
+              <th>{lan.table.tcases}</th>
+              <th>{lan.table.trec}</th>
+              <th>{lan.table.tdcases}</th>
+              <th>{lan.table.tdcases}</th>
+              <th>{lan.table.tdrec}</th>
+              <th>{lan.table.tddeaths}</th>
+              <th>{lan.table.active}</th>
+              <th>{lan.table.critical}</th>
+              <th>{lan.table.cpom}</th>
+              <th>{lan.table.dpom}</th>
+              <th>{lan.table.apom}</th>
+              <th>{lan.table.rpom}</th>
+              <th>{lan.table.crpom}</th>
+              <th>{lan.table.tests}</th>
+              <th>{lan.table.tpom}</th>
+              <th>{lan.table.ocpp}</th>
+              <th>{lan.table.odpp}</th>
+            </tr>
+          )
+        })}
+      </table>
+    </div>
+  )
 }
 export default World
