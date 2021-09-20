d9058507c4bbaaa713ba5d112d9a72851c9b922b
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-20 04:38:37 +0100

    Created footer comp

---
+++ src/UI/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {} from "./SVG"
+import styles from "./../../styles/Home.modules.css"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div></div>
+    </footer>
+  )
+}
+
+export default Footer
